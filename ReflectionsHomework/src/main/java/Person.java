e7d69a3319ff3eddf0dc493880ebaffeab2f6ff3
nickmcarow <nickmcarow@bk.ru>
2016-09-12 16:33:56 +0700

    Добавлен модуль, удалено лишнее.

---
+++ ReflectionsHomework/src/main/java/Person.java
@@ -0,0 +1,41 @@
+/**
+ * Тестовый класс
+ */
+public class Person {
+    String name;
+    String Surname;
+    int age;
+
+    public Person() {
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        Surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
